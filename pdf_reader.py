28375f1647000bc2b6da195e7ed7ecc890bf75c5
IDOTxLION <aaleensyed20@gmail.com>
2024-06-09 14:28:20 -0400

    MIN and MAX done

--- pdf_reader.py
+++ pdf_reader.py
@@ -64,7 +64,45 @@ specific_table_name = "Timing Parameters by Speed Bin"
 #find the table with the specific name
 table = df[df["Table Name"] == specific_table_name]
 table_data = table["Table"].iloc[0].extract()
-print(table_data)
+#print(table_data)
+def ExtractDesiredIndex(cat):
+    indices = []  
+    for i, inner_list in enumerate(table_data):
+        for j, word in enumerate(inner_list):
+            if word == cat:
+                indices.append([i,j])  # Store the index as a tuple (outer list index, inner list index)
+    return indices
+
+
+
+def ExtractDataOfCat(cat):
+    data = []
+    listOfIndex = ExtractDesiredIndex(cat)
+    firstIndex = listOfIndex[0][1]
+    for innerList in table_data:
+        data.append(innerList[firstIndex])
+    return data
+def CleanData(cat):
+    data = ExtractDataOfCat(cat)
+    try:
+        index = data.index(cat)
+        
+        for i in range(index + 1):
+            data.pop(0)  
+        return data
+    except ValueError:
+        return data
+
+
+
+        
+
+    
+data = CleanData("MIN")
+print(data)
+            
+
+
 
 # Symbol Parameter  Min  Max of DDR3-1066    
 # tCK(DLL_OFF)      5     -
